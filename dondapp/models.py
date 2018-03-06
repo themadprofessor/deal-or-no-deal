c6128430784ad2abdd6a3706abc14fe5dd8e55d0
Stuart Reilly <stu@reilly-family.co.uk>
2018-03-06 13:29:12 +0000

    Add Comment and Category models

--- dondapp/models.py
+++ dondapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    deal_id = models.ForeignKey()
+    user_id = models.ForeignKey()
+    creation_date = models.DateTimeField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
