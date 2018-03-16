0c9515de2bf2219eb1b630c6e2c980d41a7a4cb1
Stuart Reilly <stu@reilly-family.co.uk>
2018-03-16 14:52:40 +0000

    Add login, logout and failed login endpoints

--- dondapp/urls.py
+++ dondapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('category/', views.category, name='category'),
+    path('login/', views.login, name='login'),
+    path('failed/', views.failed, name='failed'),
+    path('logout/', views.logout, name='logout')
     ]
--- dondapp/views.py
+++ dondapp/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'dondapp/home.html')
 
+
 def about(request):
     return render(request, 'dondapp/about.html')
+
 
 def category(request):
     return render(request, 'dondapp/category.html')
 
+
+def failed(request):
+    return render(request, 'dondapp/failed.html')
+
+
 def login(request):
-    return render(request, 'dondapp/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        django_login(request, user)
+        print("Successful login")
+        return redirect("home")
+    else:
+        print("Failed login")
+        return redirect("failed")
+
+
+@login_required
+def logout(request):
+    django_logout(request)
+    return redirect("home")
