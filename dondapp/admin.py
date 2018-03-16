ee275671bcef0918a3a9f012126f7d7cbfbfe3b2
Stuart Reilly <stu@reilly-family.co.uk>
2018-03-16 13:03:52 +0000

    Change admin to use custom user model

--- dondapp/admin.py
+++ dondapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import *
+from .models import User as DondUser
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(DondUser)
 admin.site.register(Deal)
 admin.site.register(Category)
 admin.site.register(Comment)
