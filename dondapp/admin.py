b06ccfbeba08534a0a36a929e4c67a38775f20a8
Stuart Reilly <stu@reilly-family.co.uk>
2018-03-16 10:29:13 +0000

    Fix admin

--- dondapp/admin.py
+++ dondapp/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-<<<<<<< Updated upstream
 admin.site.register(User)
 admin.site.register(Deal)
 admin.site.register(Category)
 admin.site.register(Comment)
-=======
 
->>>>>>> Stashed changes
